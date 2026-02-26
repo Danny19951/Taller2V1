40ca24c02314f1554d9906f68dd2e2f5be9c1dcf
DESKTOP-KEHG5MG\danie <danieljlm19951@gmail.com>
2026-02-26 18:59:25 -0500

    changes made on 25 02 2026 atr 01:14, tested localy functioning

--- app/src/main/java/com/example/taller2/Activities/LogIn.kt
+++ app/src/main/java/com/example/taller2/Activities/LogIn.kt
@@ -1,25 +1,21 @@
 package com.example.taller2.Activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taller2.R
 
-class home : AppCompatActivity() {
+class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-
-        val btnIr = findViewById<Button>(R.id.ComienzaBtn)
-
-        btnIr.setOnClickListener {
-            val intent = Intent(this, LogIn::class.java)
-            startActivity(intent)
+        setContentView(R.layout.activity_log_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
